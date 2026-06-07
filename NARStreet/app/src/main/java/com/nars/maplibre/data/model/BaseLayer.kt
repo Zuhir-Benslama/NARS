3461deaa16ebfc5748475430fdaa620f1f3b6e4b
Zuhir-Benslama <zuhirbenslama@protonmail.com>
2026-06-07 07:07:20 +0100

    Remove NARStreet from monorepo — moved to https://github.com/Zuhir-Benslama/NARStreet

--- NARStreet/app/src/main/java/com/nars/maplibre/data/model/BaseLayer.kt
+++
@@ -1,8 +0,0 @@
-package com.nars.maplibre.data.model
-
-enum class BaseLayerType(val value: String, val displayName: String) {
-    SATELLITE("satellite", "Satellite"),
-    STREET("street", "Street"),
-    LIGHT("light", "Light"),
-    DARK("dark", "Dark")
-}
